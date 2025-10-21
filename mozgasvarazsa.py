5b64e44600411db4d8c1dcc703877a9d00a3380d
tallerpalmate <tallerpalmate@gmail.com>
2025-10-21 14:29:28 +0200

    Update app.run to specify host and port
    Change app.run to use environment PORT or default to 5000.

--- mozgasvarazsa.py
+++ mozgasvarazsa.py
@@ -19,4 +19,6 @@ def kapcsolat():
     return render_template('kapcsolat.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
